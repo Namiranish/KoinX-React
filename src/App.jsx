01ee2b5b0a3821d0c0dbe4521df91d46e9d9213d
Namirah anis <namiranish@gmail.com>
2025-06-16 02:19:17 +0530

    added the final touch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+
+import RightContainer from "./components/RightContainer";
+import React from 'react'; 
+import Tax from "./components/Tax";
+
 function App() {
- 
   return (
-    <>
-     
-    </>
-  )
+    <div className="bg-gray-100">
+      <div className="min-h-screen flex flex-col items-center justify-center px-4 py-8">
+       
+        <div className="flex flex-col sm:flex-row gap-6 max-w-5xl w-full">
+          <Tax className="w-full sm:w-1/2" />
+          <RightContainer className="w-full sm:w-1/2" />
+        </div>
+
+
+      </div>
+
+
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { FaCheck, FaChevronDown } from 'react-icons/fa';
-
+import React from 'react'; 
 const country = [
   {
     id: 1,
